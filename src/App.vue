83a08e437394e65c9ba15eca857fd768da99118a
nancy <smithnancy@outlook.com>
2023-07-02 22:56:16 +1000

    component form created, imported on app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-
+  import Form from './components/Form.vue'
 </script>
 
 <template>
-  
+  <h1 class="title">Search Weather</h1>
+    <div class="container weather-search">
+      <Form/>
+    </div>
 </template>
---
+++ src/components/Form.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+const countries = [
+    { code: 'US', name: 'United States' },
+    { code: 'AR', name: 'Argentina' },
+    { code: 'CO', name: 'Colombia' },
+    { code: 'CR', name: 'Costa Rica' },
+    { code: 'ES', name: 'Spain' },
+]
+</script>
+
+<template>
+    <form class="form">
+        <div class="field">
+            <label for="city">City</label>
+            <input
+                type="text"
+                id="city"
+                placeholder="city"
+            >
+        </div>
+        <div class="field">
+            <label for="country">Country</label>
+            <select id="country">
+                <option value="">-- Select --</option>
+                <option v-for="country in countries" :value="country.code">{{ country.name }}</option>
+            </select>  
+        </div>
+        <input type="submit" value="Check Weather">
+    </form>
+</template>
