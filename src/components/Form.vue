ea323586f56a3edb98a0adaa8672276cef723616
nancy <smithnancy@outlook.com>
2023-07-03 11:41:07 +1000

    validating  form and  displaying error

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,27 +1,57 @@
 <script setup>
+    import { reactive, ref } from 'vue';
+    import Alert from'./Alert.vue'
 
-const countries = [
-    { code: 'US', name: 'United States' },
-    { code: 'AR', name: 'Argentina' },
-    { code: 'CO', name: 'Colombia' },
-    { code: 'CR', name: 'Costa Rica' },
-    { code: 'ES', name: 'Spain' },
-]
+    const search = reactive({
+        city: '',
+        country: ''
+    })
+
+    const error = ref('')
+
+    const countries = [
+        { code: 'US', name: 'United States' },
+        { code: 'AR', name: 'Argentina' },
+        { code: 'CO', name: 'Colombia' },
+        { code: 'CR', name: 'Costa Rica' },
+        { code: 'ES', name: 'Spain' },
+    ]
+
+    const CheckWeather = () => {
+        //console.log('check weather...')
+        if (Object.values(search).includes('')) {
+            //console.log('error')
+            error.value = 'Please select city and country'
+            return
+        }
+
+        error.value = ''
+    }
+
 </script>
 
 <template>
-    <form class="form">
+    <form
+        class="form"
+        @submit.prevent="CheckWeather"
+    >
+        <Alert v-if="error">
+            {{ error }}
+        </Alert>
         <div class="field">
             <label for="city">City</label>
             <input
                 type="text"
                 id="city"
                 placeholder="city"
+                v-model="search.city"
             >
         </div>
         <div class="field">
             <label for="country">Country</label>
-            <select id="country">
+            <select id="country"
+                v-model="search.country"
+            >
                 <option value="">-- Select --</option>
                 <option v-for="country in countries" :value="country.code">{{ country.name }}</option>
             </select>  
