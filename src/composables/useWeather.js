eebbcb18fc65eeb3fe79f37f3bfeefc537f2a7a0
nancy <smithnancy@outlook.com>
2023-07-03 12:51:45 +1000

    using aios getting the values of lat and lon

--- src/composables/useWeather.js
+++ src/composables/useWeather.js
@@ -1,16 +1,27 @@
+import axios from "axios"
+
 export default function useWeather() { 
 
-    const getWeather = ({ city, country}) => {
+    const getWeather = async({ city, country}) => {
         //console.log('get weather', city ,country)
 
         //import API key
         const key = import.meta.env.VITE_API_KEY
 
         //get long  and lat
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${key}`
-        console.log(url)
+        try {
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${key}`
+            //console.log(url)
+            const { data } = await axios(url) // doing  destructure allows mw to enter directly to the data object
+            //console.log(data)// on this case returned the data  with a position 0
+            const { lon, lat } = data[0]
+            //console.log('lon: '+ lon + ' lat: '+ lat)
 
-        //get weather
+            //get weather
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return {
